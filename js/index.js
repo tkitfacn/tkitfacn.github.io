780c84da1ee0f3e3b10c35f6cc5487626372c704
Duy Quang Ph <phunghuuduyquang@gmail.com>
2020-02-24 20:45:07 +0700

    update speed scroll

--- js/index.js
+++ js/index.js
@@ -4,11 +4,13 @@ var tabletscrolls = document.querySelectorAll("#tablet_scroll");
 
 var render = function() {
     var scrolltop = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+    var speed = document.documentElement.clientHeight / 1080;
+    //console.log(windowHeight);
     scrolltop -= 20;
 
     for(var i = 0; i < laptopscrolls.length; i++) {
         var layer = laptopscrolls[i];
-        var y = (scrolltop - layer.offsetTop) * 0.15;
+        var y = (scrolltop - layer.offsetTop) * 0.15 * speed;
         layer.style.backgroundPosition = "0px " + y + "px";
         laptopPreviewes[i].style.backgroundPosition = "0px " + y + "px";
         //console.log(y);
@@ -17,16 +19,16 @@ var render = function() {
     scrolltop -= 10;
     for(var i = 0; i < tabletscrolls.length; i++) {
         var layer = tabletscrolls[i];
-        var y = (scrolltop - layer.offsetTop) * 0.2;
+        var y = (scrolltop - layer.offsetTop) * 0.2 * speed;
         layer.style.backgroundPosition = "0px " + y + "px";
         tabletPreviewes[i].style.backgroundPosition = "0px " + y + "px";
         //console.log(y);
     }
 
-    scrolltop -= 90;
+    scrolltop -= 100;
     for(var i = 0; i < phonescrolls.length; i++) {
         var layer = phonescrolls[i];
-        var y = (scrolltop - layer.offsetTop) * 0.6;
+        var y = (scrolltop - layer.offsetTop) * 0.7 * speed;
         layer.style.backgroundPosition = "0px " + y + "px";
         phonePreviewes[i].style.backgroundPosition = "0px " + y + "px";
         //console.log(y);
