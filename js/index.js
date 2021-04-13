b69649fe53d4370717657509710534a2180d8e2b
TitanDemons <phunghuuduyquang@gmail.com>
2021-04-13 12:01:07 +0700

    update content

--- js/index.js
+++ js/index.js
@@ -58,19 +58,30 @@ var updateImage = setInterval(function() {
 }, 3000);
 
 
-
-//HairCreator
+//tkitfacn
+var realchess3rd = ["url('media/images/content/realchess3rd/1.png')", "url('media/images/content/realchess3rd/2.png')", "url('media/images/content/realchess3rd/3.png')", "url('media/images/content/realchess3rd/4.png')"];
 var haircreatorImages = ["url('media/images/content/haircreator/1.png')", "url('media/images/content/haircreator/2.png')", "url('media/images/content/haircreator/3.png')"];
-var funraceImages = ["url('media/images/content/funrace/1.png')", "url('media/images/content/funrace/2.png')"];
+var moveonlinesImages = ["url('media/images/content/moveonlines/1.png')", "url('media/images/content/moveonlines/2.png')"];
 var bulletslayerImages = ["url('media/images/content/bulletslayer/1.png')", "url('media/images/content/bulletslayer/2.png')", "url('media/images/content/bulletslayer/3.png')"];
 
+//vitpr
+var tankgunImages = ["url('media/images/content/tankgun/1.png')", "url('media/images/content/tankgun/2.png')"];
+var funraceImages = ["url('media/images/content/funrace/1.png')", "url('media/images/content/funrace/2.png')"];
 
 var backgroundLoop = function() {
     timeUpdateBGColor = 1;
     var index = 0;
+
     laptopPreviewes[index].style.backgroundImage = haircreatorImages[imageCount%haircreatorImages.length];
-    phonePreviewes[index].style.backgroundImage = funraceImages[imageCount%funraceImages.length];
+    phonePreviewes[index].style.backgroundImage = realchess3rd[imageCount%realchess3rd.length];
     tabletPreviewes[index].style.backgroundImage = bulletslayerImages[imageCount%bulletslayerImages.length];
+
+    index++;
+
+    laptopPreviewes[index].style.backgroundImage = moveonlinesImages[imageCount%moveonlinesImages.length];
+    tabletPreviewes[index].style.backgroundImage = tankgunImages[imageCount%tankgunImages.length];
+    phonePreviewes[index].style.backgroundImage = funraceImages[imageCount%funraceImages.length];
+
 }
 
 var timeUpdateBGColor = 0.0;
